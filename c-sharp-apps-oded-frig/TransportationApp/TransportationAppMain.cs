57eb4d5e85dcd3ed509e98611fe2749dd6b7fa26
Oded <oded.frig1@gmail.com>
2024-06-09 02:58:36 +0300

    task 1

---
+++ c-sharp-apps-oded-frig/TransportationApp/TransportationAppMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_oded_frig.TransportationApp
+{
+    internal class TransportationAppMain
+    {
+
+
+        public static void MainEntry()
+        {
+            Console.WriteLine("TransportationApp");
+        }
+    }
+
+   
+}
