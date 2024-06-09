89808fb30809f491b499e224eb06bc56ca88ee7c
Oded <oded.frig1@gmail.com>
2024-06-09 18:05:16 +0300

    .

--- c-sharp-apps-oded-frig/common-system-shared/ProcessManager.cs
+++ c-sharp-apps-oded-frig/common-system-shared/ProcessManager.cs
@@ -1,6 +1,7 @@
 ﻿using c_sharp_apps_oded_frig.BankApp;
 using c_sharp_apps_oded_frig.DraftApp;
 using c_sharp_apps_oded_frig.SportApp;
+using c_sharp_apps_oded_frig.TransportationApp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,11 @@ namespace c_sharp_apps_oded_frig.common_system_shared
             if (choise == 1)
                 BankAppMain.MainEntry();
             else if (choise == 2)
-                DraftAppMain.MainEntry();
-            else if (choise == 3)
                 SportAppMain.MainEntry();
+            else if (choise == 3)
+                TransportationAppMain.MainEntry();
+            else if (choise == 4)
+                DraftAppMain.MainEntry();
 
 
 
